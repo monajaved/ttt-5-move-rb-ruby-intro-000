051c5175fda939c9ac515ae3524e054308a4d53a
Mona Javed Syed <monajaved93@gmail.com>
2020-10-13 09:03:24 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,9 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(num)
+  num = num.to_i
+  num = num - 1
+  # return num
+end
+# input_to_index(num)
